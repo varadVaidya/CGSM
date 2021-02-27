555aa08a80b226d91f1ccad012c7a682ece120e1
Aditya Shirwatkar <50446525+aditya-shirwatkar@users.noreply.github.com>
2021-02-27 15:04:13 +0530

    Add files via upload

---
+++ dda/line.py
@@ -0,0 +1,61 @@
+#############################################
+# @name = Aditya Shirwatkar
+# @github = aditya-shirwatkar
+# @copyright = None 
+#############################################
+
+x1 = -1; x2 = 10
+y1 = -3; y2 = 3
+
+dx = (x2-x1)
+dy = (y2-y1)
+
+step = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+
+xpp = round(dx/step, 3); ypp = round(dy/step, 3)
+
+x,y = x1,y1
+
+points = [[x,y]]
+
+print('######### INFO #########')
+print('x1 = ', x1, ' ', 'y1 = ', y1)
+print('dx = ', dx, ' ', 'dy = ', dy, ' ', 'm = ', round(dy/dx, 3))
+print('step = ', step)
+print('xpp = ', xpp, ' ', 'ypp = ', ypp)
+
+while int(x) != x2:
+    x += xpp
+    y += ypp
+
+    points.append([int(x),int(y)])
+
+print(points)
+
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import numpy as np
+
+N = 4*step
+# make an empty data set
+data = np.ones((N, N)) * np.nan
+# fill in some fake data
+for point in points:
+    data[N-1 - (N//2 + point[1]), (point[0] + N//2)] = 1
+
+# make a figure + axes
+fig, ax = plt.subplots(1, 1, tight_layout=True)
+# make color map
+my_cmap = colors.ListedColormap(['r', 'g', 'b'])
+# set the 'bad' values (nan) to be white and transparent
+my_cmap.set_bad(color='w', alpha=0)
+# draw the grid
+for x in range(N + 1):
+    ax.axhline(x, lw=2, color='k', zorder=5)
+    ax.axvline(x, lw=2, color='k', zorder=5)
+# draw the boxes
+ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, N, 0, N], zorder=0)
+# turn off the axis labels
+ax.axis('off')
+
+plt.show()
